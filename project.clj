20c26833040fd1a1e4fb11f409a96293444207e1
Maciej Jesionowski <yavnrh@gmail.com>
2014-01-15 22:37:09 +0100

    Corrected description in project file

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject tetris "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Basic tetris-like game in Clojure"
+  :url "https://github.com/yavn/tetris"
+  :license {:name "GNU General Public License"
+            :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main tetris.core)
