e6d4f8f6ad2ebadfe5feb32c6fe923f5f0acc609
Maciej Jesionowski <yavnrh@gmail.com>
2014-01-28 11:07:16 +0100

    Clean slate

--- src/tetris/core.clj
+++ src/tetris/core.clj
@@ -1,84 +1,49 @@
 (ns tetris.core
   (:gen-class)
-  (:require [tetris.grid :as grid])
   (:import [javax.swing JFrame JPanel]
            [java.awt.event KeyEvent]))
 
-(defn print-thread [s]
-  (let [thread-name (-> (Thread/currentThread) (.getName))]
-    (println thread-name s)))
+(def grid-size { :rows 14 :cols 10 })
+(def cell-size-in-pixels 32)
 
-(def shared-grid (atom nil))
-(def shared-shape (atom nil))
+(defn cells->pixels [n]
+  (* n cell-size-in-pixels))
 
-(defn make-panel []
-  (proxy [JPanel] []
-    (getPreferredSize [] grid/grid-dimension)
-    (paintComponent [g]
-      (grid/paint g (grid/place-shape @shared-grid @shared-shape)))))
+(def colors 
+  { :empty   java.awt.Color/lightGray 
+    :cyan    java.awt.Color/cyan
+    :blue    java.awt.Color/blue
+    :orange  java.awt.Color/orange
+    :yellow  java.awt.Color/yellow
+    :green   java.awt.Color/green
+    :magenta java.awt.Color/magenta
+    :red     java.awt.Color/red })
 
-(defn shape-update-pos [shape grid row-fn col-fn]
-  {:pre [(= (class shape) clojure.lang.Atom)]}
-  (swap! shape (fn [old-shape]
-                 (let [shape-grid (:grid @shape)
-                       [row col] (:position old-shape)
-                       new-pos [(row-fn row) (col-fn col)]
-                       in-bounds? (and (<= 0 (new-pos 1) (- (grid/cols grid) (grid/cols shape-grid)))
-                                       (<= (new-pos 0) (- (grid/rows grid) (grid/rows shape-grid))))]
-                   (if in-bounds?
-                     (assoc old-shape :position new-pos)
-                     old-shape)))))
-
-(defn shape-update-rotation [shape]
-  {:pre [(= (class shape) clojure.lang.Atom)]}
-  (swap! shape (fn [old-shape] (grid/rotate-shape old-shape))))
-
-(defn make-input-handler [panel]
-  (proxy [java.awt.event.KeyListener] []
-    (keyPressed [e]
-      (let [key-code (.getKeyCode e)]
-        (cond
-          (= key-code KeyEvent/VK_LEFT) (shape-update-pos shared-shape @shared-grid identity dec)
-          (= key-code KeyEvent/VK_RIGHT) (shape-update-pos shared-shape @shared-grid identity inc)
-          (= key-code KeyEvent/VK_UP) (shape-update-rotation shared-shape)))      
-      (.repaint panel))
-    (keyReleased [_])
-    (keyTyped [_])))
-
-(defn make-update-task [panel]
-  (proxy [java.util.TimerTask] []
-    (run []
-      (shape-update-pos shared-shape @shared-grid inc identity)
-      (.repaint panel))))
-
-(defn make-frame []
-  (let [panel (make-panel)
-        timer (java.util.Timer.)]
-    (doto (JFrame. "Clojure Tetris")
-      (.add panel)
-      (.pack)
-      (.addKeyListener (make-input-handler panel))
-      (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE)
-      (.addWindowListener
-        (proxy [java.awt.event.WindowListener] []
-          (windowActivated [_])
-          (windowClosed [_]
-            (.cancel timer))
-          (windowClosing [_])
-          (windowDeactivated [_])
-          (windowDeiconified [_])
-          (windowIconified [_])
-          (windowOpened [_])))
-      (.setVisible true))
-    (.scheduleAtFixedRate timer (make-update-task panel) 1000 1000)))
-
-(defn game-reset! []
-  (reset! shared-grid (grid/make-grid))
-  (reset! shared-shape (grid/make-random-shape)))
+(def shapes
+  [
+   {:color :cyan
+    :grid ["XXXX"]}
+   {:color :blue
+    :grid ["X__"
+           "XXX"]}
+   {:color :orange
+    :grid ["__X"
+           "XXX"]}
+   {:color :yellow
+    :grid ["XX"
+           "XX"]}
+   {:color :green
+    :grid ["_XX"
+           "XX_"]}
+   {:color :magenta
+    :grid ["_X_"
+           "XXX"]}
+   {:color :red
+    :grid ["XX_"
+           "_XX"]}
+   ])
   
 (defn -main
-  [& args]
-  ;; work around dangerous default behaviour in Clojure
-  (alter-var-root #'*read-eval* (constantly false))
-  (game-reset!)
-  (make-frame))
+ [& args]
+ ;; work around dangerous default behaviour in Clojure
+ (alter-var-root #'*read-eval* (constantly false)))
