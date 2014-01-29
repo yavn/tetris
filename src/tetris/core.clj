ec91b3c50c2cb6e84d153b9382732da016df75a0
Maciej Jesionowski <yavnrh@gmail.com>
2014-01-29 22:29:36 +0100

    Grid rotation and some comments

--- src/tetris/core.clj
+++ src/tetris/core.clj
@@ -1,15 +1,23 @@
+;;;; Copyright 2014 Maciej Jesionowski
+;;;; A tetris-like game in Clojure
+;;;; This is purely for educational purposes. Comments are a bit excessive
+;;;; because of that.
+;;;;
+;;;; Licensed under GNU GPL v3
+
+
 (ns tetris.core
   (:gen-class)
   (:import [javax.swing JFrame JPanel]
            [java.awt.event KeyEvent]))
 
-(def grid-size { :rows 14 :cols 10 })
+(def game-grid-size { :rows 14 :cols 10 })
 (def cell-size-in-pixels 32)
 
 (defn cells->pixels [n]
   (* n cell-size-in-pixels))
 
-(def colors 
+(def colors
   { :empty   java.awt.Color/lightGray 
     :cyan    java.awt.Color/cyan
     :blue    java.awt.Color/blue
@@ -19,9 +27,12 @@
     :magenta java.awt.Color/magenta
     :red     java.awt.Color/red })
 
+;; These are the shapes of blocks used in the game.
+;; Convenient string definitions will be later converted
+;; to a grid representation.
+
 (def shapes
-  [
-   {:color :cyan
+  [{:color :cyan
     :grid ["XXXX"]}
    {:color :blue
     :grid ["X__"
@@ -40,10 +51,17 @@
            "XXX"]}
    {:color :red
     :grid ["XX_"
-           "_XX"]}
-   ])
+           "_XX"]}])
 
-(defn make-grid [rows cols]
+;; Game data structures are typical Clojure collections.
+;; I define a number of interface functions to manipulate these
+;; structures, e.g. create, query etc.
+;; I'm not sure if using records or something else is preferable.
+
+(defn make-grid
+  "Creates an empty grid which is a collection of rows,
+  each cell filled with an :empty keyword."
+  [rows cols]
   (repeat rows (repeat cols :empty)))
 
 (defn grid-rows [grid]
@@ -52,14 +70,41 @@
 (defn grid-cols [grid]
   (count (first grid)))
 
-(defn make-grid-with-shape [shape]
+(defn make-grid-with-shape
+  "Takes a shape definition and returns a grid representing that shape.
+  Shape must have this structure (example):
+    {:color :red
+     :grid  [\"XXX\"
+             \"_X_\"]}
+  Cells marked X will become a color and underscore will become :empty area."
+  [shape]
   (let [color (:color shape)
         grid (:grid shape)]
   (for [row grid] (replace {\X color, \_ :empty}
                            row))))
 
-(defn rotate-grid [grid]
-  grid)
+(defn rotate-grid
+  "Perform a clockwise rotation of the grid. This is similar to matrix
+  transposition but also reverses the order of the rows."
+  [grid]
+  (apply map vector (reverse grid)))
+  ;;
+  ;; This is so terse that we should break it apart and see what's going on.
+  ;;
+  ;; grid - is a vector of vectors, e.g. [[1 2 3][4 5 6]]
+  ;; (reverse grid) - reverses elements of a collection, thus we get: [[4 5 6][1 2 3]]
+  ;; apply - we use it to treat a collection as a list of arguments to a function.
+  ;; (apply map vector [[4 5 6][1 2 3]]) - here map is the invoked function,
+  ;;   vector is the first argument to map, and the vector [[4 5 6][1 2 3]] becomes
+  ;;   the remaining argument list to map (always the last argument to apply).
+  ;;   Apply breaks apart the outer vector as if we wrote this instead:
+  ;;   (map vector [4 5 6] [1 2 3]).
+  ;; map - takes a function (vector in this case) and applies it to first elements
+  ;;   of each collection, then the second elements, etc. This gives us:
+  ;;   ((vector 4 1)(vector 5 2)(vector 6 3))
+  ;; vector - takes a number of arguments and turns them into a vector
+  ;;
+  ;; Wow. So little code to accomplish so much :)
 
 (defn -main
  [& args]
